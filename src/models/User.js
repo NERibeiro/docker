3e714dbda1942d1059f43a2d56a6506f7d09932b
NERibeiro <renanpvh@hotmail.com>
2024-08-28 23:48:37 -0400

    att

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,31 @@
 
-import Sequelize, {model} from 'sequelize'
+import Sequelize, {Model} from 'sequelize'
 
 class User extends Model {
     static init(sequelize){
-        super.init( {
-
+        super.init(
+    
+        {
+            name:{
+                type:Sequelize.STRING,
+                allowNull: false
+              },
+            
+              age: {
+                type:Sequelize.INTEGER,
+                allowNull: false
+              },
+            
+              email: {
+                type:Sequelize.STRING,
+                allowNull: false,
+                unique: true
+              },
         }
         , 
             
             {sequelize})
     }
 }
+
+export default User
--- src/server.js
+++ src/server.js
@@ -1,11 +1,33 @@
 
 import express from 'express'
+import User from './models/User.js'
+import Sequelize from 'sequelize'
+import config  from './config/database.js'
 import userRoutes from './routes.js'
 
 
 const app = express()
 app.use(express.json())
 
+const sequelize = new Sequelize(config)
+User.init(sequelize)
+
+
 app.use('/usuarios', userRoutes)
 
-app.listen(3000, () => console.log("servidor on"))
+
+sequelize
+.authenticate()
+.then(
+    () => {
+        console.log('Banco de dados conectado')
+        app.listen(3000, () => 
+            {console.log("servidor on")
+            })
+    }
+)
+.catch( (err) => {
+    console.error(err)
+})
+
+
