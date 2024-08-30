418b719e9ec4df74ba092a12d2bc74b531f37866
NERibeiro <renanpvh@hotmail.com>
2024-08-29 23:26:10 -0400

    attu

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,25 @@
-export const createUser = (req, res) => {
+import User from '../models/User.js'
+import crypto from 'node:crypto'
 
-    res.status(200).json({ message: 'Tudo Ok'})
+export const createUser = async (req, res) => {
+
+    const userToCreate = {
+        id: crypto.randomUUID(),
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email
+    }
+
+
+    const user = await User.create(req.body)
+  
+    res.status(201).json(user)
 }
+
 
 export const getAllUsers = (req, res) => {
 
-    res.status(200).json({ message: 'Tudo Ok'})
+    res.status(200).json({message: 'OK'})
 }
 
 export const deleteUser = (req, res) => {
